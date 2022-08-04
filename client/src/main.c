787497d197520e3e45ece392209f14ff45343633
Aviad Bagno <aviad.bagno@gmail.com>
2022-08-04 15:26:56 +0300

    in client - getting the other peer's address

--- client/src/main.c
+++ client/src/main.c
@@ -1,4 +1,28 @@
-int main()
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+
+#include <stdio.h>
+#include <netinet/in.h>
+
+#include "p2p.h"
+
+int main(void)
 {
+    struct sockaddr_in peer_addr;
+    if (get_other_peer_addr(&peer_addr) < 0)
+    {
+        printf("Network error when trying to get other's peer address");
+        return -1;  
+    }
+    printf("peer: %s:%d\n", inet_ntoa(peer_addr.sin_addr), ntohs(peer_addr.sin_port));
+
     return 0;
 }
