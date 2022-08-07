82540648f6a770407468461beae89f4df55d131d
Aviad Bagno <aviad.bagno@gmail.com>
2022-08-07 17:09:45 +0300

    Testing connection between peers

--- client/src/main.c
+++ client/src/main.c
@@ -16,6 +16,11 @@
 
 int main(void)
 {
+    int our_socket = 0;
+    char *message = "Hello peer";
+    char buffer[300];
+    socklen_t size;
+
     struct sockaddr_in peer_addr;
     if (get_other_peer_addr(&peer_addr) < 0)
     {
@@ -23,6 +28,30 @@ int main(void)
         return -1;
     }
     printf("peer: %s:%d\n", inet_ntoa(peer_addr.sin_addr), ntohs(peer_addr.sin_port));
+
+    if (udp_hole_punching(peer_addr) < 0)
+    {
+        printf("Network error when trying to hole punch");
+        return -1;
+    }
+
+    if ((our_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) /* Creates the socket */
+        return -1;
+
+    /* Setup an entry for the peer on MY nat */
+    if (sendto(our_socket, (const char *)&message, strlen(message),
+               MSG_CONFIRM, (const struct sockaddr *)&peer_addr,
+               sizeof(peer_addr)) /* Sends the massage to the server */
+        < 0)
+        return -1;
+
+    size = sizeof(peer_addr);
+    if (recvfrom(our_socket, buffer, 300, MSG_WAITALL, (struct sockaddr *)&peer_addr,
+                 &size) /* Gets the other peer's address from the server */
+        < 0)
+        return -1;
+
+    printf("Got: %s\n", buffer);
 
     return 0;
 }
