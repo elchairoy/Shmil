fcc6910ca6274a16102b455a6202593bdd6e8887
elchairoy <67311284+elchairoy@users.noreply.github.com>
2022-08-07 15:10:18 +0300

    Update p2p.c

--- client/src/p2p.c
+++ client/src/p2p.c
@@ -41,24 +41,3 @@ int get_other_peer_addr(struct sockaddr_in *peer_addr)
     close(our_socket); 
     return 0; 
 }
-
-int get_other_peer_addr(struct sockaddr_in *peer_addr)
-{
-    int our_socket = 0;
-    struct sockaddr_in server_addr;
-
-    if ((our_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
-        return -1;
-
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(SERVER_PORT);
-    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
-
-    if (connect(our_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-        return -1;
-
-    if (!read(our_socket, peer_addr, sizeof(*peer_addr)))
-        return -1;
-
-    return 0;
-}
